7c04ca9ca5613b1c29104ffe33952be98c07d6f5
SACHIN-85 <sachin5616975@gmail.com>
2025-04-09 18:00:02 +0530

    Initial Commit

---
+++ src/main/java/Sb/for_git/SbForGitApplication.java
@@ -0,0 +1,13 @@
+package Sb.for_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbForGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Sb/for_git/SbForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package Sb.for_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbForGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
